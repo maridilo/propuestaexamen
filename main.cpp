3dda6cda65c57972357151b86d74d1c026e2bb34
maridilo <146822054+maridilo@users.noreply.github.com>
2023-11-07 12:14:26 +0100

    commimt

--- main.cpp
+++ main.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
+#include <cmath>
 #include <iomanip>
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
-    for (int a = 2; a < 9; a++) {
-        std::cout << a << std::endl;
-        printf("a=%d\n", a);
-    }
-    for (int b = 3; b < 16; b++) {
-        std::cout << b << std::endl;
-        printf("b=%d\n", b);
-    }
+int numero;
+int i;
 
+std::cout << "Por favor, ingrese un número: " << std::endl;
+std::cin >> numero;
+
+std::cout << "Tabla de multiplicar del " << numero << " utilizando un bucle for:" << std::endl;
+
+for (i = 1; i <= 10; i++) {
+std::cout << numero << " x " << i << " = " << numero * i << std::endl;
+}
+
+int main() {
+
+    for a in range(3,5,6,8);
+
+    for b in range(4,8,12,15);
+
+    c = sqrt(a**2 + b**2);
+    if c.is_integer() and a + b > c:
+    print(f"Triple Pitagórico: ({a}, {b}, {int(c)}")
+
+} else {
+std::cout << "No es un numero pitagorico" ;
+}
     return 0;
 }
+
